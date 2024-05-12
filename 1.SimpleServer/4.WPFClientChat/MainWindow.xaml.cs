81e23dbb33bbd9664a4e2d9d78808f3bc50134c6
novakvova <novakvova@gmail.com>
2024-05-12 13:06:20 +0300

    Upload image to server

--- 1.SimpleServer/4.WPFClientChat/MainWindow.xaml.cs
+++ 1.SimpleServer/4.WPFClientChat/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using Newtonsoft.Json;
+using System.IO;
+using System.Net;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,6 +15,11 @@ using System.Windows.Shapes;
 
 namespace _4.WPFClientChat
 {
+
+    public class UploadResponseDTO
+    {
+        public string Image { get; set; } = string.Empty;
+    }
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -23,6 +32,38 @@ namespace _4.WPFClientChat
 
         private void btnPhotoSelect_Click(object sender, RoutedEventArgs e)
         {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.ShowDialog();
+            var filePath = dlg.FileName;
+            var bytes = File.ReadAllBytes(filePath);
+            var base64 = Convert.ToBase64String(bytes);
+            string json = JsonConvert.SerializeObject(new
+            {
+                Photo = base64
+            });
+            bytes = Encoding.UTF8.GetBytes(json);
+            string serverUrl = "https://npr211.itstep.click";
+            WebRequest request = WebRequest.Create($"{serverUrl}/api/Gallery/upload");
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            using(var stream = request.GetRequestStream())
+            {
+                stream.Write(bytes, 0, bytes.Length);
+            }
+            try
+            {
+                var response = request.GetResponse();
+                using (var stream = new StreamReader(response.GetResponseStream()))
+                {
+                    string data = stream.ReadToEnd();
+                    var resp = JsonConvert.DeserializeObject<UploadResponseDTO>(data);
+                    MessageBox.Show(serverUrl+resp.Image);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
 
         }
     }
