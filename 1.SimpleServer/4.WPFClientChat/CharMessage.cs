05b8fb6ac32758910182a26900659fba123768ea
novakvova <novakvova@gmail.com>
2024-05-12 10:20:33 +0300

    Begin WPF Chat

---
+++ 1.SimpleServer/4.WPFClientChat/CharMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.WPFClientChat
+{
+    public class CharMessage
+    {
+        //Id - користувача в чаті
+        public required string UserId { get; set; }
+        public required string Name { get; set; }
+        public required string Text { get; set; }
+        public required string Photo { get; set; }
+    }
+}
